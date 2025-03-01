2b6f21b603e134cda362bdeee2778b84042676db
Dane <daneurchillbro@gmail.com>
2025-02-28 23:54:02 -0600

    Update Hero.tsx

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 export const Hero: React.FC = () => {
   return (
-    <header className="mb-[164px] max-md:mb-20 max-sm:mb-10" role="banner">
-      <h1 className="text-[64px] text-[#1E1E1E] font-normal max-md:text-5xl max-sm:text-4xl">
+    <header className="mb-8 md:mb-12" role="banner">
+      <h1 className="text-4xl md:text-5xl lg:text-6xl text-[#1E1E1E] font-normal">
         Find everything in stock.
       </h1>
-      <h2 className="text-[64px] text-[#1E1E1E] font-normal max-md:text-5xl max-sm:text-4xl">
+      <h2 className="text-4xl md:text-5xl lg:text-6xl text-[#1E1E1E] font-normal">
         In one place.
       </h2>
     </header>
