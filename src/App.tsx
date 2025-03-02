5a8dafd1cfd193beba1a23916ffc6649cfc9014b
Dane <daneurchillbro@gmail.com>
2025-03-02 12:46:00 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,11 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-
-// Import only the pages you've actually created
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Products from "./pages/Products";
+import News from "./pages/News";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
-// Add other imports as you create those pages
 
 const queryClient = new QueryClient();
 
@@ -20,15 +21,11 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/products" element={<Products />} />
           <Route path="/news" element={<News />} />
           <Route path="/privacy" element={<PrivacyPolicy />} />
-          <Route path="*" element={<NotFound />} />
-          <Route path="/privacy" element={<PrivacyPolicy />} />
-          {/* Keep your existing catch-all route */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
